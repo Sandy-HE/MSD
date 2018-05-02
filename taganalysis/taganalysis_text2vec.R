69b796633c84bebb687cc2609ebbaa6def67a3f5
SANDY HE <13035338@adsroot.uts.edu.au>
2018-05-02 10:47:09 +1000

    change single word analysis to phrase analysis

--- taganalysis/taganalysis_text2vec.R
+++ taganalysis/taganalysis_text2vec.R
@@ -23,7 +23,10 @@ itemsMultiTrans <- function(items){
   itemlist <- tstrsplit(items, "\\*")
   itemdf <- as.data.frame(itemlist, stringsAsFactors = F,col.names = c("item","popularity"))
   if(!is.integer(itemdf$popularity)) print(itemdf)
-  itemdf$multiItem <- strrep(paste0(itemdf$item," "),as.integer(itemdf$popularity) )
+  #for single word
+  #itemdf$multiItem <- strrep(paste0(itemdf$item," "),as.integer(itemdf$popularity) )
+  #for phrase
+  itemdf$multiItem <- strrep(paste0(itemdf$item,";"),as.integer(itemdf$popularity) )
   itemdf <- itemdf[itemdf$popularity!=0,]
   
   itemsstr<-paste(itemdf$multiItem,collapse =" ")
@@ -38,7 +41,7 @@ obsv1 <- sapply(obsv, itemsMultiTrans)    #char vector
 
 #data backup
 alltags$transtags <- obsv1
-fwrite(alltags,file = "alltranstags.csv")
+fwrite(alltags,file = "alltranstags_phrase.csv")
 
 
 #prepare train set and test set
@@ -46,12 +49,19 @@ train = alltags[alltags$track_id %in% train_ids,]
 test = alltags[alltags$track_id %in% test_ids,]  
 
 #==== text2vec: generate term-co-occurance matrix ====
-tok_fun <- word_tokenizer  
-it_train <- itoken(train$transtags,   
+#use word_tokenizer for single word analysis
+#tok_fun <- word_tokenizer
+#it_train <- itoken(train$transtags,   
                   #preprocessor = prep_fun,   
-                  tokenizer = tok_fun,   
-                  ids = train$track_id,   
-                  progressbar = T)  
+#                  tokenizer = tok_fun,   
+#                  ids = train$track_id,   
+#                  progressbar = T)  
+
+#use space tokenizer for pharase analysis by separating items through ";"
+tokens <- space_tokenizer(train$transtags, sep=";")
+it_train <- itoken(tokens, 
+                   ids = train$track_id,   
+                   progressbar = T) 
 
 #set stop words and remove them from vocabulary
 stop_words <- c("the","to","of","and","a","in","this","-","for","it","that","on","is")
@@ -76,74 +86,98 @@ system.time(tcm_train <- create_tcm(it_train, vectorizer, skip_grams_window = 5L
 
 
 #prune matrix only containing topN-freq tags 
+#for small sample
 tcm_sample <- tcm_train[rownames(tcm_train) %in% topwords,colnames(tcm_train) %in% topwords]
 tcm_sample_mat <- as.matrix(tcm_sample)
+#for whole train set
+#tcm_sample_mat <- as.matrix(tcm_train)
 
 tcm_sample_diag <- diag(diag(tcm_sample_mat))
 tcm_sample_whole <- tcm_sample_mat + t(tcm_sample_mat) - tcm_sample_diag*2
-tcm_sample_whole <- round(tcm_sample_whole,2)
-tcm_sample_df <- as.data.frame(tcm_sample_whole)
+tcm_sample_whole <- round(tcm_sample_whole,2)   # this is matrix
+tcm_sample_df <- as.data.frame(tcm_sample_whole, stringsAsFactors = F)
+words <- rownames(tcm_sample_whole)
+words <- order(words)
+tcm_sample_whole <- tcm_sample_whole[words,words]
 
 tags <- rownames(tcm_sample_df)
 tcm_sample_df<- cbind(tags,tcm_sample_df)
 fwrite(tcm_sample_df, file = "tcm_sample_pruned_vocab.csv")
 
 #==== visualization ====
-library(igraph)
 set.seed(10)
-#sampling a small data size
-samplemat <- tcm_sample_whole[1:10,1:10]
-sampletags <- as.data.frame(rownames(samplemat))
-colnames(sampletags) <- "tags"
-sampledf <- graph.adjacency(samplemat,weighted=TRUE)
-sampledf <- get.data.frame(sampledf)
 
 #use the whole data size
-topwordsnode <- as.data.frame(topwords,stringsAsFactors = F)
-topwordsnode <- topwordsnode %>% rowid_to_column("id")
+topwordsnode <- as.data.frame(rownames(tcm_sample_whole),stringsAsFactors = FALSE)
+#topwordsnode <- as.data.frame(topwords,stringsAsFactors = FALSE)
+colnames(topwordsnode) <- "tags"
+#topwordsnode <- topwordsnode %>% rowid_to_column("id")
+topwordsnode$id <- rownames(topwordsnode)
 topwordsedge <- graph.adjacency(tcm_sample_whole,weighted=TRUE)
 topwordsedge <- get.data.frame(topwordsedge)
 
+fwrite(topwordsnode, file="items_node.csv")
+fwrite(topwordsedge, file="items_edge.csv")
 #====igraph package usage ====
+library(igraph)
 #This is one visualization style, but not what I want
 #network <- graph_from_data_frame(d = sampledf, vertices = sampletags, directed = FALSE)
 #plot(network,layout = layout_with_graphopt)
-network <- graph_from_data_frame(d = topwordsedge, vertices = topwordsnode, directed = T)
-plot(network, edge.arrow.size = 0.2,layout = layout_with_graphopt)
+igrahnet <- graph_from_data_frame(d = topwordsedge, vertices = topwordsnode, directed = T)
+plot(igrahnet, edge.arrow.size = 0.2,layout = layout_with_graphopt)
 #==== End of igraph package usage ====
-
-#==== network package usage ====
-library(network)
-tagsnetwork <- network(topwordsedge, vertex.attr = topwordsnode, matrix.type = "edgelist", ignore.eval = FALSE)
-plot(tagsnetwork, vertex.cex = 3,mode = "circle")
-#==== End of network package usage ====
 
 
 #==== tidygraph and ggraph ====
 library(tidygraph)
-nettidy <- tbl_graph(nodes = sampletags, edges = sampledf, directed = T)
-nettidy <- tbl_graph(nodes = topwordsnode, edges = topwordsedge, directed = T)
 library(ggraph)
+#nettidy <- tbl_graph(nodes = sampletags, edges = sampledf, directed = T)
+nettidy <- tbl_graph(nodes = topwordsnode, edges = topwordsedge, directed = T)
+
 ggraph(nettidy, layout = "graphopt") + 
   geom_node_point() +
   geom_edge_link(aes(width = weight), alpha = 0.2) + 
   scale_edge_width(range = c(0.02, 0.1)) +
-  geom_node_text(aes(label = topwords), repel = TRUE) +
+  geom_node_text(aes(label = tags), repel = TRUE) +
   labs(edge_width = "correlation strength") +
   theme_graph()
 #==== End of tidygraph and ggraph ====
 
+#==== network package usage ====
+library(network)
+tagsnetwork <- network(topwordsedge, vertex.attr = topwordsnode, matrix.type="edgelist")
+#tagsnetwork <- network(tcm_sample_whole, matrix.type="adjacency")
+plot(tagsnetwork, vertex.cex = 3,mode = "circle")
+#==== End of network package usage ====
+
 #==== ggnetwork usage ====
+#reuse network() to get tagsnetwork
 library(ggplot2)
 library(ggnetwork)
-ggplot(topwordsedge, aes(from, to)) +
-  geom_edges(aes(color = weight)) +
-  geom_nodes(color = "grey50") +
-  geom_nodelabel(aes(size = weight,label=from),
-                 color = "grey20") +
+library(intergraph)
+library(tnet)
+#e <- network.edgecount(tagsnetwork)
+#set.edge.attribute(tagsnetwork, "type", sample(letters[24:26], e, replace = TRUE))
+set.edge.attribute(tagsnetwork, "weight", topwordsedge$weight)
+t <- as.edgelist(tagsnetwork, attrname = "weight")%>%
+  as.tnet %>%
+  degree_w
+set.vertex.attribute(tagsnetwork, "degree_w", t[, "output" ])
+
+l <- tagsnetwork %v% "degree_w"
+
+l <- network.vertex.names(tagsnetwork)
+set.vertex.attribute(tagsnetwork, "label", l)
+
+gn <- ggnetwork(tagsnetwork)
+
+ggplot(gn, aes(x, y, xend=xend, yend=yend)) +
+  geom_edges(aes(color = log(weight))) +
+  geom_nodes(color = "grey50",mode="circle") +
+  geom_nodelabel(aes(label=label,size = degree_w),
+                 color = "grey20",label.size = NA) +
   scale_size_continuous(range = c(2, 8)) +
   scale_color_gradient2(low = "grey25", midpoint = 0.75, high = "black") +
   guides(size = FALSE, color = FALSE) + 
   theme_blank()
-
 
